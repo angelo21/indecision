0c24c988e73181a08ba0f6381567e26eb07cb742
Tom LanFranca <ittechtommy@gmail.com>
2018-02-27 15:16:08 -0600

    Setup babel

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//JSX = JavaScript XML
+
+var template = React.createElement(
+  "h1",
+  null,
+  "Indecision app"
+);
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+
+//JSX = JavaScript XML
+
+var template = <h1>Indecision app</h1>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
