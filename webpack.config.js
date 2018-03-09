f01f7248b7ae8e9d45cac2c8751e335e404364e4
Tom LanFranca <ittechtommy@gmail.com>
2018-03-09 07:46:29 -0600

    Set up scss and normalize.css to begin styling app

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
       loader: "babel-loader",
       test: /\.js$/, //tells babel to look of files ending in .js
       exclude: /node_modules/
+    }, {
+      test: /\.s?css$/, //tells webpack to look for changes to scss files. The question mark allows webpack to look for scss and css files. The s is optional
+      use: [
+        "style-loader",
+        "css-loader",
+        "sass-loader"
+      ]
     }]
   },
   devtool: "cheap-module-eval-source-map", //shows where errors occur in dev tools. What file as opposed to the final output bundle.
